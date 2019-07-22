aaf46c4b395eec6a631b6ecf139307a3b5ca7fa4
Teddy Sung <teddysung168@gmail.com>
2019-07-22 22:01:01 +0800

    The second version counting the turns you guess

---
+++ guessing2.py
@@ -0,0 +1,15 @@
+import random
+r = random.randint(1, 100)
+turn = 0
+while True:
+	ans = input('請猜數字')
+	ans = int(ans)
+	turn = turn + 1
+	if ans == r:
+		print('猜對了。討厭，以後不跟你玩了')
+		print('你猜了', turn, '次')
+		break
+	elif ans > r:
+		print('傻瓜，你猜的太大了，哈哈!')
+	else:
+		print('阿呆，你猜得太小了，ㄎㄎ!')
